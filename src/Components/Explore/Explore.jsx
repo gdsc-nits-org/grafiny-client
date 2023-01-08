c5cd88add471461162584e324f3a18b9dbc14040
Anubhav <chakrabortyanubhav0@gmail.com>
2023-01-08 15:45:06 +0530

    whitespace commit undo

--- src/Components/Explore/Explore.jsx
+++ src/Components/Explore/Explore.jsx
@@ -1,33 +1,29 @@
-import React from 'react';
-import style from './Explore.module.scss';
-import image from './exploreimg.png';
+import React from "react";
+import style from "./Explore.module.scss";
+import image from "./exploreimg.png";
 
-const Explore = ()=> {
+const Explore = () => {
   return (
     <div className={style.explorecontainer}>
       <div className={style.upperhalf}>
-        <span className={style.exploretext}>
-          Explore |
-        </span>
+        <span className={style.exploretext}>Explore |</span>
         <span className={style.collegetext}>
           &nbsp;<span>Subjects</span>
         </span>
       </div>
       <div className={style.bottomhalf}>
         <div className={style.searchcontainer}>
-        <div className={style.img}>
-           <img src={image}  alt="exploreimg" />
+          <div className={style.img}>
+            <img src={image} alt="exploreimg" />
+          </div>
+          <div className={style.search}>
+            <input type="text" placeholder="What are you looking for ?" />
+          </div>
         </div>
-      <div className={style.search}>
-        <input type="text" placeholder='What are you looking for ?' />
-      </div>
-      </div>
-      <div className={style.searchbutton}>
-          Search
-      </div>
+        <div className={style.searchbutton}>Search</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Explore;
