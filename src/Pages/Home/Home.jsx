4ec4794d3c67e659bf8e426b89433b558427f2e7
Pratik Majumdar <63517465+codadept@users.noreply.github.com>
2023-01-08 15:13:54 +0530

    ADD: Simple Git Hooks Pre-Commit rule (#5)
    * ADD: Simple Git Hook

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import style from "./Home.module.scss";
 const Home = () => {
   return (
     <main className={style.home}>
-      <h1>Home Page</h1>
+      <h1>Home Page for Grafiny</h1>
       <Button type="button" />
     </main>
   );
