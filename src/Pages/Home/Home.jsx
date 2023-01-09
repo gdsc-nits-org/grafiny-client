b5bbf7ffd35076308f5bd7d16f9b1a20ca2264d0
Pratik Majumdar (codadept) <info.pratikmajumdar@gmail.com>
2023-01-09 20:53:45 +0530

    DEPLOY: To trigger deployment preview

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import style from "./Home.module.scss";
 const Home = () => {
   return (
     <main className={style.home}>
-      <h1>Home Page for Grafiny</h1>
+      <h1>Home Page for Grafiny by GDSC NITS</h1>
       <Button type="button" />
       <div>
         <Explore />
