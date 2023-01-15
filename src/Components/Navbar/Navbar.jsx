3042c6a767896292d74519d61452b5c782edfdc7
masoom <masoomsahu20@gmail.com>
2023-01-15 16:19:05 +0530

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,67 @@
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
+import styles from "./Navbar.module.scss";
+
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  const [toggle, setToggle] = useState(false);
+
+  const handleSwitch = () => {
+    setToggle((prevToggleValue) => !prevToggleValue);
+  };
+
+  const changeSwitch = () => {
+    if (window.scrollY >= 90 && window.innerWidth > 1000) {
+      setToggle(true);
+    } else {
+      setToggle(false);
+    }
+  };
+  window.addEventListener("scroll", changeSwitch);
+
+  return (
+    <nav className={toggle ? `${styles.navbar} ${styles.large}` : styles.navbar}>
+      <Link className={styles.logopart} to="/">
+        <img src="/images/gdsclogo.png" alt="logo" />
+        <img className={styles.logo2} src="/images/grafinyicon.png" alt="logo" />
+      </Link>
+      <div className={styles.menupart}>
+        <button className={styles.switchIcon} onClick={handleSwitch}>
+          {toggle ? (
+            <Icon icon="maki:arrow" width="37"></Icon>
+          ) : (
+            <Icon icon="charm:menu-hamburger" width="37" height="37"></Icon>
+          )}
+        </button>
+        <div className={styles.links}>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `${styles.navlinks} ${styles.active}` : styles.navlinks
+            }
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `${styles.navlinks} ${styles.active}` : styles.navlinks
+            }
+            to="/resources"
+          >
+            Resources
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `${styles.navlinks} ${styles.active}` : styles.navlinks
+            }
+            to="/profile"
+          >
+            Profile
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,12 @@
-import { Button } from "../../Components";
+// import { Button } from "../../Components";
 
 import style from "./Home.module.scss";
 
 const Home = () => {
   return (
     <main className={style.home}>
-      <h1>Home Page for Grafiny</h1>
-      <Button type="button" />
+      {/* <h1>Home Page for Grafiny</h1>
+      <Button type="button" /> */}
     </main>
   );
 };
