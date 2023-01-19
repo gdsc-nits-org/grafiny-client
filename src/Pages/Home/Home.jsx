b9182cc38d34c0e3c80fa2726c819aa0774e60c5
Pratik Majumdar (codadept) <info.pratikmajumdar@gmail.com>
2023-01-19 13:24:37 +0530

    FIX: Minor tweaks

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,11 @@
-import { Button, Explore, Landing } from "../../Components";
+import { Explore, Landing } from "../../Components";
 import style from "./Home.module.scss";
 
 const Home = () => {
   return (
     <main className={style.home}>
-      <div>
-        <Landing />
-      </div>
-      <Button type="button" />
-      <div>
-        <Explore />
-      </div>
+      <Landing />
+      <Explore />
     </main>
   );
 };
