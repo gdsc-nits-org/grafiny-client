39332d6b5f81b74eee4980967cf25e13881a4fc0
Hrishikesh Borah <96012098+HrishB16@users.noreply.github.com>
2023-01-19 12:57:44 +0530

    Landing Component (#10)
    * initial commit
    
    * Development changes and bugs fixes
    
    * Package.lock.json deleted
    
    * re4moved .npmrc
    
    * design changes
    
    * made changes to font sizes
    
    * development changes

---
+++ src/Components/Landing/Landing.jsx
@@ -0,0 +1,33 @@
+import Landingstyle from "./Landing.module.scss";
+
+const Landing = () => {
+  return (
+    <>
+      <div className={Landingstyle.header}>
+        <div className={Landingstyle.title}>
+          <div className={Landingstyle.text}>
+            <h1>Grafiny</h1>
+            <h3>Lorem ipsum dolor sit amet consectetur.</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa pariatur harum
+              maiores? Vitae repellendus, cumque voluptatibus iure itaque accusantium.
+              Delectus, rem. Quae consequuntur voluptatum nam optio fugiat alias aliquam,
+              vitae explicabo eos quibusdam praesentium, perspiciatis laboriosam a
+              voluptatem saepe officiis.
+            </p>
+          </div>
+        </div>
+
+        <div className={Landingstyle.imageLand}>
+          <img src="/assets/Group.png" alt="Grafiny Landing" />
+        </div>
+      </div>
+
+      <div className={Landingstyle.buttonLand}>
+        <button className={Landingstyle.btn}>Get started</button>
+      </div>
+    </>
+  );
+};
+
+export default Landing;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
-import { Button, Explore } from "../../Components";
+import { Button, Explore, Landing } from "../../Components";
 import style from "./Home.module.scss";
 
 const Home = () => {
   return (
     <main className={style.home}>
-      <h1>Home Page for Grafiny by GDSC NITS</h1>
+      <div>
+        <Landing />
+      </div>
       <Button type="button" />
       <div>
         <Explore />
