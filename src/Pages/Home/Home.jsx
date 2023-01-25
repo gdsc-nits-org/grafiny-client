cdf9b54a319384cd76869a2e73d34f6415cd2aad
Anubhav <chakrabortyanubhav0@gmail.com>
2023-01-25 15:17:47 +0530

    FIX: explore page image position

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,12 @@
-import { Button } from "../../Components";
+import { Button, Explore, Landing } from "../../Components";
 import Institutions from "../../Components/Institutions/Institutions";
-import { Explore, Landing } from "../../Components";
 import style from "./Home.module.scss";
 
 const Home = () => {
   return (
     <main className={style.home}>
       <h1>Home Page for Grafiny</h1>
-      <Button type="button" />  
+      <Button type="button" />
       <Landing />
       <Explore />
       <Institutions />
