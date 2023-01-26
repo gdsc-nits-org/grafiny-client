f787c7bcb28bc41efd0faedab670d8cc2dbc8b32
Hrishikesh Borah <hrishikeshborahofficial@gmail.com>
2023-01-26 18:03:29 +0530

    ADDED: Topics

---
+++ src/Pages/Topic/Topic.jsx
@@ -0,0 +1,12 @@
+import Topics from "../../Components/Topics/Topics";
+import styles from "./Topics.module.scss";
+
+const Topic = () => {
+  return (
+    <main className={styles.topic}>
+      <Topics />
+    </main>
+  );
+};
+
+export default Topic;
