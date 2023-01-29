e380df8c1f0d9462f9059a321b112ac5cc6a4ebe
masoom <masoomsahu20@gmail.com>
2023-01-29 17:15:10 +0530

    FIX: resolve conflict  with homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,7 @@
-// import { Button } from "../../Components";
-
 import style from "./Home.module.scss";
 
 const Home = () => {
-  return (
-    <main className={style.home}>
-      {/* <h1>Home Page for Grafiny</h1>
-      <Button type="button" /> */}
-    </main>
-  );
+  return <main className={style.home}></main>;
 };
 
 export default Home;
