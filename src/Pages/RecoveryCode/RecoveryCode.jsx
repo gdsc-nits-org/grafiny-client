e9f4b70d004728867b8fc70a827e566de27519ed
Pragya Annesha Baruah <pragya.annesha@gmail.com>
2023-03-22 02:50:15 +0530

    UPDATE: Recovery code page

--- src/Pages/RecoveryCode/RecoveryCode.jsx
+++ src/Pages/RecoveryCode/RecoveryCode.jsx
@@ -3,26 +3,34 @@ import { v4 as uuidv4 } from "uuid";
 import styles from "./RecoveryCode.module.scss";
 
 const RecoveryCode = () => {
+  const inputIds = Array(6)
+    .fill()
+    .map(() => uuidv4());
+
   const [otp, setOtp] = useState(Array(6).fill(""));
+
   const refs = useRef([]);
+
   const handleChange = (e, index) => {
     if (Number.isNaN(Number(e.target.value))) return false;
     setOtp([...otp.map((d, idx) => (idx === index ? e.target.value : d))]);
     if (e.target.nextSibling && e.target.value !== "") {
-      e.target.nextSibling.focus();
+      e.target.blur();
+      if (index === otp.length - 1) {
+        refs.current[index].focus();
+      } else {
+        e.target.nextSibling.focus();
+      }
     }
     return true;
   };
+
   const handleKeyUp = (e) => {
     if (e.key === "Backspace" && e.target.previousSibling) {
       e.target.previousSibling.focus();
     }
     return true;
   };
-
-  const inputIds = Array(6)
-    .fill()
-    .map(() => uuidv4());
 
   return (
     <div className={styles.wrapper}>
@@ -35,7 +43,8 @@ const RecoveryCode = () => {
               className={styles.otp}
               type="text"
               maxLength="1"
-              key={inputIds[index]}
+              key={inputIds.index}
+              data-index={index}
               value={data}
               onChange={(e) => handleChange(e, index)}
               onKeyUp={(e) => handleKeyUp(e, index)}
