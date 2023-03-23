cc7b599e6878d343ddf5a23be5768cc1412ee1bc
masoom <masoomsahu20@gmail.com>
2023-03-23 12:28:27 +0530

    FIX: Nav Bug

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,41 +18,29 @@ const Navbar = () => {
   return (
     <nav className={toggle ? `${styles.navbar} ${styles.large}` : styles.navbar}>
       <div className={styles.leftSection}>
-        <div>
-          <button className={styles.switchIcon} onClick={handleSwitch}>
-            {toggle ? (
-              <Icon
-                icon="mdi:arrow-left"
-                width="37"
-                height="37"
-                color="rgb(116, 114, 114)"
-              />
-            ) : (
-              <Icon
-                icon="charm:menu-hamburger"
-                width="35"
-                height="35"
-                color="rgb(116, 114, 114)"
-              ></Icon>
-            )}
-          </button>
-        </div>
-        <div className={styles.logopart}>
-          <Link to="/">
-            <img className={styles.logo1} src="/images/gdsclogo.png" alt="logo" />
-            <img className={styles.logo2} src="/images/grafinyicon.png" alt="logo" />
-          </Link>
-        </div>
+        <button className={styles.switchIcon} onClick={handleSwitch}>
+          {toggle ? (
+            <Icon
+              icon="mdi:arrow-left"
+              width="37"
+              height="37"
+              color="rgb(116, 114, 114)"
+            />
+          ) : (
+            <Icon
+              icon="charm:menu-hamburger"
+              width="35"
+              height="35"
+              color="rgb(116, 114, 114)"
+            ></Icon>
+          )}
+        </button>
+        <Link className={styles.logopart} to="/">
+          <img className={styles.logo1} src="/images/gdsclogo.png" alt="logo" />
+          <img className={styles.logo2} src="/images/grafinyicon.png" alt="logo" />
+        </Link>
       </div>
-      <div className={styles.menupart}>
-        <div className={styles.navProfile}>
-          <Icon
-            icon="healthicons:ui-user-profile-outline"
-            width="37"
-            height="37"
-            color="rgb(116, 114, 114)"
-          />
-        </div>
+      <div className={styles.right}>
         <div className={styles.links}>
           <NavLink
             className={({ isActive }) =>
@@ -66,9 +54,9 @@ const Navbar = () => {
             className={({ isActive }) =>
               isActive ? `${styles.navlinks} ${styles.active}` : styles.navlinks
             }
-            to="/resources"
+            to="/bookmark"
           >
-            Resources
+            Bookmark
           </NavLink>
           <NavLink
             className={({ isActive }) =>
@@ -78,6 +66,22 @@ const Navbar = () => {
           >
             Profile
           </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `${styles.navlinks} ${styles.active}` : styles.navlinks
+            }
+            to="/team"
+          >
+            Team
+          </NavLink>
+          <Link className={styles.navProfile} to="/">
+            <Icon
+              icon="healthicons:ui-user-profile-outline"
+              width="37"
+              height="37"
+              color="rgb(116, 114, 114)"
+            />
+          </Link>
         </div>
       </div>
     </nav>
