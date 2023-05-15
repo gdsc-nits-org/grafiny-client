78e540e92163eb1007ea8b6c6a52eeaf25c3d552
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-05-15 14:12:56 +0530

    ADD: Dropdown features for course and topic selction

--- src/Pages/Upload/Upload.jsx
+++ src/Pages/Upload/Upload.jsx
@@ -1,13 +1,31 @@
 /* eslint-disable no-param-reassign */
 import React, { useState } from "react";
 import { Icon } from "@iconify/react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Upload.module.scss";
 
 const UploadingPage = () => {
   const [files, setFiles] = useState([]);
   const [uploadedFiles, setUploadedFiles] = useState([]);
-  const [uploaded, setUploaded] = useState(false);
   const [selected, setSelected] = useState(false);
+  const [buttonstate, setbuttonState] = useState(false);
+  const [selectedCourse, setSelctedCourse] = useState("");
+  // const [topics, setTopics] = useState([]);
+  const [uploaded, setUploaded] = useState(false);
+  const [selectedTopic, setSelectedTopic] = useState("");
+  const courseOptions = [
+    { name: "Mathematics", id: 1 },
+    { name: "Theory of Computation", id: 2 },
+    { name: "Computer Architecture and Organization", id: 3 },
+  ];
+
+  const topicOptions = [
+    { name: "Random Variables", id: 1, courseId: 1 },
+    { name: "Finite Automata", id: 2, courseId: 2 },
+    { name: "Pumping Lemma", id: 3, courseId: 2 },
+    { name: "Paging and Segmentation", id: 4, courseId: 3 },
+    { name: "Random Process", id: 5, courseId: 1 },
+  ];
   const handleDrop = (e) => {
     e.preventDefault();
     const newFiles = [...files];
@@ -29,29 +47,30 @@ const UploadingPage = () => {
     });
     setFiles(newFiles);
     setSelected(true);
+    setbuttonState(true);
   };
 
   const handleDeleteUploaded = async (fileName) => {
-    try {
-      await fetch(`/api/deleteFile?fileName=${fileName}`, {
-        method: "DELETE",
-      });
-      setUploadedFiles((prevSelectedFiles) =>
-        prevSelectedFiles.filter((file) => file.name !== fileName)
-      );
-    } catch (error) {
-      // console.error(error);
-    }
+    // try {
+    //   await fetch(`/api/deleteFile?fileName=${fileName}`, {
+    //     method: "DELETE",
+    //   });
+    setUploadedFiles((prevSelectedFiles) =>
+      prevSelectedFiles.filter((file) => file.name !== fileName)
+    );
+    // } catch (error) {
+    //   // console.error(error);
+    // }
   };
-
+  const navigate = useNavigate();
   const handleDelete = (file) => {
     const newFiles = files.filter((f) => f.name !== file.name);
     setFiles(newFiles);
   };
-
+  let uploadedFileCount = 0;
   const handleUpload = () => {
     const newFiles = [...files];
-    let uploadedFileCount = 0;
+
     newFiles.forEach((file) => {
       // const formData = new FormData();
       // formData.append("file", file);
@@ -74,42 +93,55 @@ const UploadingPage = () => {
         if (uploadedFileCount === newFiles.length) {
           setFiles([]);
           setSelected(false);
+          navigate("/profile");
         }
       };
       xhr.onerror = () => {};
       xhr.send(formData);
-      // end of temporary code
-
-      // Using fetch api
-      // fetch("/iuuji", {
-      //   method: "POST",
-      //   body: formData,
-      //   // Progress tracking with Fetch API
-      //   onUploadProgress: (e) => {
-      //     if (e.lengthComputable) {
-      //       file.progress = Math.round((e.loaded / e.total) * 100);
-      //       setFiles([...newFiles]);
-      //     }
-      //   },
-      // })
-      //   .then((response) => {
-      //     if (response.ok) {
-      //       file.progress = 100;
-      //       setUploadedFiles((prevUploadedFiles) => [...prevUploadedFiles, file]);
-      //       uploadedFileCount += 1;
-      //       if (uploadedFileCount === newFiles.length) {
-      //         setFiles([]);
-      //         setSelected(false);
-      //       }
-      //     } else {
-      //       // handle error
-      //     }
-      //   })
-      //   .catch(() => {
-      //     // handle error
-      //   });
     });
     setUploaded(true);
+    // end of temporary code
+
+    // Using fetch api
+    // fetch("/iuuji", {
+    //   method: "POST",
+    //   body: formData,
+    //   // Progress tracking with Fetch API
+    //   onUploadProgress: (e) => {
+    //     if (e.lengthComputable) {
+    //       file.progress = Math.round((e.loaded / e.total) * 100);
+    //       setFiles([...newFiles]);
+    //     }
+    //   },
+    // })
+    //   .then((response) => {
+    //     if (response.ok) {
+    //       file.progress = 100;
+    //       setUploadedFiles((prevUploadedFiles) => [...prevUploadedFiles, file]);
+    //       uploadedFileCount += 1;
+    //       if (uploadedFileCount === newFiles.length) {
+    //         setFiles([]);
+    //         setSelected(false);
+    //       }
+    //     } else {
+    //       // handle error
+    //     }
+    //   })
+    //   .catch(() => {
+    //     // handle error
+    //   });
+  };
+  const handleCourseChange = (e) => {
+    const courseId = e.target.value;
+    setSelctedCourse(courseId);
+    setbuttonState(false);
+    // fetch(`/api/topics?courseId=${courseId}`)
+    //   .then((response) => response.json())
+    //   .then((data) => setTopics(data));
+  };
+  const handleTopicChange = (e) => {
+    const topicId = e.target.value;
+    setSelectedTopic(topicId);
   };
 
   return (
@@ -119,7 +151,45 @@ const UploadingPage = () => {
       onDragOver={(e) => e.preventDefault()}
     >
       <h1>Upload</h1>
-      <div className={styles["file-upload"]}>
+
+      <div className={styles["dropdown-container"]}>
+        <label htmlFor="course">Select Course</label>
+        <select id="course" value={selectedCourse} onChange={handleCourseChange}>
+          <option value=" ">--SELECT COURSE--</option>
+          {courseOptions.map((course) => (
+            <option key={course.id} value={course.id}>
+              {course.name}
+            </option>
+          ))}
+          ;
+        </select>
+      </div>
+
+      <div className={styles["dropdown-container"]}>
+        <label htmlFor="topic">Select Topic</label>
+        <select
+          id="topic"
+          value={selectedTopic}
+          onChange={handleTopicChange}
+          disabled={!selectedCourse}
+        >
+          <option value=" ">--SELECT TOPIC--</option>
+          {topicOptions.map(
+            (topic) =>
+              Number(topic.courseId) === Number(selectedCourse) && (
+                <option key={topic.id} value={topic.id}>
+                  {topic.name}
+                </option>
+              )
+          )}
+        </select>
+      </div>
+
+      <div
+        className={`${
+          files.length > 0 || uploaded ? styles["file-uploaded"] : styles["file-upload"]
+        }`}
+      >
         <label htmlFor="file-upload-input">
           <div className={styles["upload-box"]}>
             <div className={styles["upload-icon"]}>
@@ -139,12 +209,13 @@ const UploadingPage = () => {
           onChange={handleBrowse}
         />
       </div>
+
       <h1
         className={` ${
           selected && files.length > 0 ? styles.uploadStatus : styles.uploadStatusHide
         }`}
       >
-        Uploading-n/n files
+        Uploading-{uploadedFileCount}/{files.length} files
       </h1>
 
       <div className={styles["uploaded-files"]}>
@@ -194,7 +265,14 @@ const UploadingPage = () => {
         ))}
       </div>
       <div className={styles["upload-button-container"]}>
-        <button className={styles["upload-button"]} onClick={handleUpload}>
+        <button
+          className={`{ ${
+            !buttonstate || uploaded
+              ? styles["disabled-upload-button"]
+              : styles["upload-button"]
+          }`}
+          onClick={handleUpload}
+        >
           UPLOAD FILES
         </button>
       </div>
