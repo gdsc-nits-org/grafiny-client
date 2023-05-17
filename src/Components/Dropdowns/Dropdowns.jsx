74868152d90fe05971cea54e2eae64fdab0daaac
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-05-17 17:32:55 +0530

    ADD: topics-courses relationship

--- src/Components/Dropdowns/Dropdowns.jsx
+++ src/Components/Dropdowns/Dropdowns.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import styles from "./Dropdowns.module.scss";
 
 const Dropdown = ({ label, value, onChange, disabled, options, selectedCourse }) => {
+  let newOptions = options;
+  if (label === "Topic") {
+    newOptions = options.filter(
+      (topic) => Number(topic.courseId) === Number(selectedCourse)
+    );
+  }
   return (
     <div className={styles["dropdown-container"]}>
       <label htmlFor="course">
-        {" "}
-        Select {label} <span className={styles["compulsory-field"]}>*</span>{" "}
+        Select {label} <span className={styles["compulsory-field"]}>*</span>
       </label>
       <select
         id="course"
@@ -16,16 +21,7 @@ const Dropdown = ({ label, value, onChange, disabled, options, selectedCourse })
         className={styles["dropdown-content"]}
       >
         <option value=" ">--Select {label}--</option>
-        if({label}===Topic)
-        {options.map(
-          (topic) =>
-            Number(topic.courseId) === Number(selectedCourse) && (
-              <option key={topic.id} value={topic.id}>
-                {topic.name}
-              </option>
-            )
-        )}
-        {options.map((option) => (
+        {newOptions.map((option) => (
           <option key={option.id} value={option.id}>
             {option.name}
           </option>
