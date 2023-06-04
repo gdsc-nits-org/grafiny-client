38f3286feefbfd8af448500c29c02658cb431ddf
masoom <masoomsahu20@gmail.com>
2023-06-04 23:12:17 +0530

    FIX:navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import styles from "./Navbar.module.scss";
+
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const handleSwitch = () => {
     setToggle((prevToggleValue) => !prevToggleValue);
   };
+
   const changeSwitch = () => {
     if (window.scrollY >= 90 && window.innerWidth > 1000) {
       setToggle(true);
@@ -14,11 +16,12 @@ const Navbar = () => {
       setToggle(false);
     }
   };
+
   window.addEventListener("scroll", changeSwitch);
+
   return (
-    <nav className={toggle ? `${styles.navbar} ${styles.large}` : styles.navbar}>
+    <nav className={`${styles.navbar} ${toggle ? styles.large : ""}`}>
       <div className={styles.leftSection}>
-        <div>
           <button className={styles.switchIcon} onClick={handleSwitch}>
             {toggle ? (
               <Icon
@@ -68,9 +71,57 @@ const Navbar = () => {
             <img className={styles.logo2} src="/images/grafinyicon.png" alt="logo" />
           </Link>
         </div>
-      </div>
+          {toggle ? (
+            <Icon
+              icon="mdi:arrow-left"
+              width="37"
+              height="37"
+              color="rgb(116, 114, 114)"
+            />
+          ) : (
+            <Icon
+              icon="charm:menu-hamburger"
+              width="35"
+              height="35"
+              color="rgb(116, 114, 114)"
+            ></Icon>
+          )}
+        </button>
+        <Link className={styles.logopart} to="/">
+          <img className={styles.logo1} src="/images/gdsclogo.png" alt="logo" />
+          <img className={styles.logo2} src="/images/grafinyicon.png" alt="logo" />
+        </Link>
+        <button className={styles.switchIcon} onClick={handleSwitch}>
+          {toggle ? (
+            <Icon
+              icon="mdi:arrow-left"
+              width="37"
+              height="37"
+              color="rgb(116, 114, 114)"
+            />
+          ) : (
+            <Icon
+              icon="charm:menu-hamburger"
+              width="35"
+              height="35"
+              color="rgb(116, 114, 114)"
+            />
+          )}
+        </button>
+        <Link className={styles.logopart} to="/">
+          <img className={styles.logo1} src="/images/gdsclogo.png" alt="logo" />
+          <img className={styles.logo2} src="/images/grafinyicon.png" alt="logo" />
+        </Link>
+      <Link className={styles.navProfile2} to="/">
+        <Icon
+          icon="healthicons:ui-user-profile-outline"
+          width="37"
+          height="37"
+          color="rgb(116, 114, 114)"
+        />
+      </Link>
       <div className={styles.right}>
-        <div className={styles.links}>
+        <div className={`${styles.links} ${toggle ? "showDropdown" : ""}`}>
           <NavLink
             className={({ isActive }) =>
               isActive ? `${styles.navlinks} ${styles.active}` : styles.navlinks
@@ -116,4 +167,5 @@ const Navbar = () => {
     </nav>
   );
 };
+
 export default Navbar;
