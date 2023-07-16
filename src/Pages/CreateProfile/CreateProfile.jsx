a39cbd21515504d7a3daf10dc0233368512fe4f5
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-07-16 22:53:29 +0530

    ADD: pfp option

--- src/Pages/CreateProfile/CreateProfile.jsx
+++ src/Pages/CreateProfile/CreateProfile.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Icon } from "@iconify/react";
 import styles from "./CreateProfile.module.scss";
 const CreateProfile = () => {
   const [selectedYear, setSelectedYear] = useState("");
@@ -9,14 +10,25 @@ const CreateProfile = () => {
   const handleInstChange = (e) => {
     setSelectedInst(e.target.value);
   };
+  const handleFileChange = () => {
+    //
+  };
   return (
     <div className={styles["create-profile"]}>
       <div className={styles["profile-form"]}>
-        <h3>Create Profile</h3>
+        <div className={styles["profile-title"]}>
+          <h3>Create Profile</h3>
+          <div className={styles["profile-icon"]}>
+            <Icon className={styles["profile-icon"]} icon="iconamoon:profile-fill" />
+          </div>
+        </div>
+
         <div className={styles["profile-info"]}>
           <div className={styles["select-details"]}>
-            <h4>Select Year</h4>
-            <select value={selectedYear} onChange={handleYearChange}>
+            <h4>
+              Select Year <span className={styles["compulsory-field"]}>*</span>
+            </h4>
+            <select value={selectedYear} onChange={handleYearChange} required>
               <option value="">Select Year</option>
               <option value="1st">1st</option>
               <option value="2nd">2nd</option>
@@ -26,16 +38,36 @@ const CreateProfile = () => {
           </div>
 
           <div className={styles["select-details"]}>
-            <h4>Select Institution</h4>
-            <select value={selectedInst} onChange={handleInstChange}>
+            <h4>
+              Select Institution <span className={styles["compulsory-field"]}>*</span>
+            </h4>
+            <select value={selectedInst} onChange={handleInstChange} required>
               <option value="">Select Institution</option>
               <option value="1st">National Institute of Technology, Silchar</option>
             </select>
           </div>
 
           <div className={styles["select-details"]}>
-            <h4>Student ID</h4>
-            <input type="text" name="scholarId" pattern="\d{7}" />
+            <h4>
+              Student ID <span className={styles["compulsory-field"]}>*</span>
+            </h4>
+            <input type="text" name="scholarId" pattern="\d{7}" required />
+          </div>
+
+          <div className={styles["select-details"]}>
+            <h4 className={styles.pfp}>Add Profile Photo</h4>
+            <label htmlFor="profile-photo-input" className={styles["file-input-label"]}>
+              Choose File
+            </label>
+            <input
+              type="file"
+              id="profile-photo-input"
+              name="profilePhoto"
+              accept="image/*"
+              onChange={handleFileChange}
+              className={styles["file-input"]}
+              disabled // kept disabled for now
+            />
           </div>
         </div>
         <button className={styles["continue-button"]}>Continue</button>
