5c37d9705f5113570f4733226941a0485cd1e657
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2024-02-16 22:37:47 +0530

    Integrate materials (#49)
    * integrate materials
    
    * uniform styling on all pages
    
    * uniform styling on all pages 2.0

--- src/Components/Material/Materials.jsx
+++ src/Components/Material/Materials.jsx
@@ -1,63 +1,17 @@
 import React from "react";
-import { Icon } from "@iconify/react";
 import style from "./Materials.module.scss";
 
-const record = [
-  {
-    caption: "Material 123",
-  },
-
-  {
-    caption: "Material 123",
-  },
-
-  {
-    caption: "Material 123",
-  },
-  {
-    caption: "Material 123",
-  },
-
-  {
-    caption: "Material 123",
-  },
-
-  {
-    caption: "Material 123",
-  },
-
-  {
-    caption: "Material 123",
-  },
-
-  {
-    caption: "mMaterial 123",
-  },
-];
-
-const Materials = () => {
+const Materials = ({ name }) => {
   return (
-    <>
-      <div className={style.iconarrow}>
-        <Icon icon="mdi:arrow-left" width="37" height="37" color="rgb(116, 114, 114)" />
+    <div className={style.folders}>
+      <div className={style.foldersbox}>
+        <img src="./images/folder.png" alt="" className="" />
+        <div>
+          {" "}
+          <p className={style.materialtitle}> {name}</p>
+        </div>
       </div>
-      <div className={style.materialcontainer}>
-        <p>Materials</p>
-      </div>
-      <div className={style.folders}>
-        {record.map((data) => {
-          return (
-            <div className={style.foldersbox}>
-              <img src="./images/folder.png" alt="" className="" />
-              <div>
-                {" "}
-                <p className={style.materialtitle}> {data.caption}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </>
+    </div>
   );
 };
 
--- src/Pages/Material/Material.jsx
+++ src/Pages/Material/Material.jsx
@@ -1,11 +1,111 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { Icon } from "@iconify/react";
+import { toast } from "react-toastify";
+import UserContext from "../../Global/Auth/authContext";
 import { Materials } from "../../Components";
-
+import Loading from "../../Components/Loading/Loading";
 import style from "./Material.module.scss";
 
 const Matetial = () => {
+  const { auth } = useContext(UserContext);
+  const { state } = useLocation();
+  const context = useContext(UserContext);
+  const [items, setItems] = useState([]);
+  const { loading, setLoading, user } = context;
+  const navigate = useNavigate();
+  // const [showPopup, setShowPopup] = useState(false);
+
+  const navigateTo = () => {
+    navigate(-1);
+  };
+
+  // const togglePopup = () => {
+  //   setShowPopup(!showPopup);
+  // };
+
+  const handleItems = async () => {
+    try {
+      setLoading(() => true);
+      const token = await auth.currentUser.getIdToken(true);
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/items/allitems?id=${state.topicId}`,
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const { data } = response;
+      if (data.status !== 200) {
+        setLoading(() => false);
+        return toast.error(data.msg, { autoClose: 1200 });
+      }
+      setItems(() => data.msg.items);
+      setLoading(() => false);
+      return null;
+    } catch (error) {
+      // console.error(error);
+      setLoading(() => false);
+      return toast.error("Something Went Wrong", { autoClose: 1200 });
+    }
+  };
+
+  // const handleFiles = async (data) => {
+  //   try {
+  //     navigate(`/`, {
+  //       state: {
+  //         fileId: data.id,
+  //         fileUrl: data.url,
+  //       },
+  //     });
+  //     return null;
+  //   } catch (error) {
+  //     return toast.error("Something Went Wrong", { autoClose: 1200 });
+  //   }
+  // };
+
+  useEffect(() => {
+    if (!user) {
+      toast.error("Please Log In", { autoClose: 1200 });
+    } else if (!state) {
+      navigate("/");
+    } else {
+      handleItems();
+    }
+  }, []);
+
   return (
     <section className={style.material}>
-      <Materials />
+      {loading === false ? (
+        <div>
+          <div className={style.arrowContainer}>
+            <Icon icon="mdi:arrow-left" onClick={navigateTo} className={style.arrow} />
+            <h2 className={style.dhead}>Materials</h2>
+            {/* <button
+              className={style["add-sem"]}
+              onClick={togglePopup}
+              aria-label="Add Department"
+            >
+              {showPopup ? <Icon icon="mdi:close" /> : <Icon icon="mdi:plus" />}
+            </button> */}
+          </div>
+          <div className={style.itemsContainer}>
+            {items?.map((item) => (
+              <div
+                //  onClick={() => handleCourse(item)}
+                //  onKeyDown={() => handleCourse(item)}
+                key={item.id}
+              >
+                <Materials name={item.name} />
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <Loading />
+      )}
     </section>
   );
 };
